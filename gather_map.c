627907a0cd715c7c13f35f6c5f41ed154f5f73ff
antoine.hartwig <antoine.hartwig@epitech.eu>
2017-01-30 20:40:44 +0100

    verif plutot complete

--- gather_map.c
+++ gather_map.c
@@ -3,19 +3,26 @@
 int	check_buffer(char *buffer)
 {
   int	i;
+  int	j;
 
+  j = 7;
   i = 0;
   while (buffer[i] != '\0')
     {
+      if (((i % j) == 0) && (i != 0))
+	if (buffer[i] != '\n')
+	  return (-1);
       if ((buffer[i] >= 'A' && buffer[i] <= 'H')
 	  || (buffer[i] >= '1' && buffer[i] <= '8')
 	  || (buffer[i] == ':')
-	  || (buffer[i] == '\0'))
+	  || (buffer[i] == '\n'))
 	i++;
       else
 	return (-1);
+      j++;
     }
-  my_putchar('D');
+  if (i != 32)
+    return (-1);
   return (0);
 }
 
@@ -31,9 +38,7 @@ int	gather_map(char *file)
   if (read(fd, buff, 1024) == -1)
     return (-1);
   close(fd);
-  my_putchar('L');
   if (check_buffer(buff) == -1)
     return (-1);
-  my_putchar('M');
   return (0);
 }
